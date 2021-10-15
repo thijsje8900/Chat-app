d015eae72f44a7bf59cd5e2a3019a441a3e6ccde
Thijs Drykoningen <thijsdrykoningen@hotmail.nl>
2021-10-15 16:53:31 +0200

    Updated styling
    Styling for MijnRapportfolio. added dependencies: Bootstrap & fontawesome

--- client/main.js
+++ client/main.js
@@ -14,13 +14,10 @@ socket.on('message', function(text) {
   if (!text) {
     return;
   }
-  var container = document.querySelector('section');
+  var container = document.querySelector('#section');
   var newMessage = document.createElement('p');
   newMessage.innerText = text;
   container.appendChild(newMessage);
-
-  var seperator = document.createElement('br');
-  container.appendChild(seperator);
 
   container.scrollTop = container.scrollHeight;
 });
